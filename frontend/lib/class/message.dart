66337358676deae1154630724e237e4a96364f37
Rovalen Joy Calaguing <99468139+rovalen-joy@users.noreply.github.com>
2024-05-03 13:00:18 +0800

    created an instance of message from JSON object

--- frontend/lib/class/message.dart
+++ frontend/lib/class/message.dart
@@ -3,13 +3,22 @@ class Message {
   final String content;
 
   // Constructor
-  Message({required this.sender, required this.content});
+  Message(
+      {required this.sender,
+      required this.content}); // A constructor requiring 'sender' and 'content' to initialize a Message object
 
   factory Message.fromJson(Map<String, dynamic> json) {
+    String? sender =
+        json['sender']; // attempts to retrieve the sender from the JSON map
+    String? content = json[
+        'res']; // attempts to retrieve the content using key 'res' from the JSON map
+    if (sender == null || content == null) {
+      throw Exception(
+          'Received incomplete message data'); // throws an exception if any of them is null indicating incomplete data
+    }
     return Message(
-        // the sender and res are the name of the
-        // params returned by Flask
-        sender: json['sender'],
-        content: json['content']);
+        sender: sender,
+        content:
+            content); // creates a new Message instance using the retrieved values if they are not null
   }
 }
